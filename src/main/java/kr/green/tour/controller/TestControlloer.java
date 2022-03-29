396738cca41e71ef683d64b01a833b8f1144ea95
Jihye Jang <zihae5442@gmail.com>
2022-03-29 13:32:09 +0900

    공공데이터 넣는 중

--- src/main/java/kr/green/tour/controller/TestControlloer.java
+++ src/main/java/kr/green/tour/controller/TestControlloer.java
@@ -12,6 +12,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import kr.green.tour.vo.PlaceVO;
+
 @Controller
 public class TestControlloer {
 public ModelAndView sampleTest3(ModelAndView mv) {
@@ -20,14 +22,11 @@ public ModelAndView sampleTest3(ModelAndView mv) {
 	}
 	@RequestMapping("/place/public")
 	public ModelAndView sampleTest4(ModelAndView mv) throws Exception {
-		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6460000/jnLodgeist"); /*URL*/
-		 urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=EODB6lSechoAnAMvpQKYezu%2FdtmCs16raW%2FRkvk7q84z%2BZUybJRwBKlJAS%2Bumw%2FEWBBwsgxuaA%2F%2FsptCm7pw6g%3D%3D"); /*Service Key*/  
-		//페이지정보. 테스트는 1페이지로함.
-	    urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-	    //한페이지에서 보여지는 결과물 갯수
-	    urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-	    //숙박고유코드
-	    urlBuilder.append("&" + URLEncoder.encode("lodgeId","UTF-8") + "=" + URLEncoder.encode("3105", "UTF-8")); /*고유코드(숙박 목록의 고유코드)*/
+		 StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6460000/jnLodgeist/getNdLodgeList"); /*URL*/
+	        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=EODB6lSechoAnAMvpQKYezu%2FdtmCs16raW%2FRkvk7q84z%2BZUybJRwBKlJAS%2Bumw%2FEWBBwsgxuaA%2F%2FsptCm7pw6g%3D%3D"); /*Service Key*/
+	        //urlBuilder.append("&" + URLEncoder.encode("lodgeId","UTF-8") + "=" + URLEncoder.encode("3105", "UTF-8")); /*고유코드(숙박 목록의 고유코드)*/
+	        urlBuilder.append("&" + URLEncoder.encode("pageSize","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*페이지 크기(기본10)*/
+	        urlBuilder.append("&" + URLEncoder.encode("startPage","UTF-8") + "=" + URLEncoder.encode("0", "UTF-8")); /*시작 페이지(기본0)*/
 
 
     DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
@@ -38,7 +37,7 @@ public ModelAndView sampleTest3(ModelAndView mv) {
 		doc.getDocumentElement().normalize();
 		System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 	// 파싱할 tag
-		NodeList nList = doc.getElementsByTagName("item");
+		NodeList nList = doc.getElementsByTagName("list");
 		//System.out.println("파싱할 리스트 수 : "+ nList.getLength());
 
 		for(int temp = 0; temp < nList.getLength(); temp++){
@@ -49,6 +48,10 @@ public ModelAndView sampleTest3(ModelAndView mv) {
 				System.out.println("######################");
 				//System.out.println(eElement.getTextContent());
 				System.out.println("주소 : " + getTagValue("lodgeAddr", eElement));
+				System.out.println("지역 : " + getTagValue("lodgeZoneNm", eElement));
+				//PlaceVO place = new PlaceVO();
+				//place.setAddress1(getTagValue("lodgeAddr", eElement));
+				//placeService.insertPlace(place);
 				
 			}	// for end
 		}	// if end
