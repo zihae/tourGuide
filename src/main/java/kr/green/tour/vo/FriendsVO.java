a26e753a78fe4d3013c14d9c0e2b07138802d2f1
Jihye Jang <zihae5442@gmail.com>
2022-04-12 11:56:41 +0900

    Create FriendsVO.java

---
+++ src/main/java/kr/green/tour/vo/FriendsVO.java
@@ -0,0 +1,11 @@
+package kr.green.tour.vo;
+
+import lombok.Data;
+
+@Data
+public class FriendsVO {
+	private int friends_num;
+	private String user_id;
+	private String friend_id;
+	private String friend_name;
+}
